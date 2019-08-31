47f3043d2f9959a8cd93b9da962594d3d1496efa
richardt22 <richardtrinh22@gmail.com>
2019-08-30 21:30:15 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
